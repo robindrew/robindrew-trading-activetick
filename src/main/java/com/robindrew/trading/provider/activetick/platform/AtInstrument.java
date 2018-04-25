728ae8b876d99810f953ff1a5cea4c0491af9674
Robin Drew <robin.drew@gmail.com>
2018-04-25 23:59:32 +0100

    Renaming instruments

--- src/main/java/com/robindrew/trading/provider/activetick/platform/AtInstrument.java
+++ src/main/java/com/robindrew/trading/provider/activetick/platform/AtInstrument.java
@@ -10,32 +10,32 @@ import com.robindrew.trading.Instruments;
 public class AtInstrument extends Instrument implements IAtInstrument {
 
 	/** AUD/USD. */
-	public static final AtInstrument AUD_USD = new AtInstrument("#AUD/USD", Instruments.AUD_USD);
+	public static final AtInstrument SPOT_AUD_USD = new AtInstrument("#AUD/USD", Instruments.AUD_USD);
 	/** EUR/JPY. */
-	public static final AtInstrument EUR_JPY = new AtInstrument("#EUR/JPY", Instruments.EUR_JPY);
+	public static final AtInstrument SPOT_EUR_JPY = new AtInstrument("#EUR/JPY", Instruments.EUR_JPY);
 	/** EUR/USD. */
-	public static final AtInstrument EUR_USD = new AtInstrument("#EUR/USD", Instruments.EUR_USD);
+	public static final AtInstrument SPOT_EUR_USD = new AtInstrument("#EUR/USD", Instruments.EUR_USD);
 	/** GBP/USD. */
-	public static final AtInstrument GBP_USD = new AtInstrument("#GBP/USD", Instruments.GBP_USD);
+	public static final AtInstrument SPOT_GBP_USD = new AtInstrument("#GBP/USD", Instruments.GBP_USD);
 	/** USD/JPY. */
-	public static final AtInstrument NZD_USD = new AtInstrument("#NZD/USD", Instruments.NZD_USD);
+	public static final AtInstrument SPOT_NZD_USD = new AtInstrument("#NZD/USD", Instruments.NZD_USD);
 	/** USD/CAD. */
-	public static final AtInstrument USD_CAD = new AtInstrument("#USD/CHF", Instruments.USD_CAD);
+	public static final AtInstrument SPOT_USD_CAD = new AtInstrument("#USD/CHF", Instruments.USD_CAD);
 	/** USD/CHF. */
-	public static final AtInstrument USD_CHF = new AtInstrument("#USD/CHF", Instruments.USD_CHF);
+	public static final AtInstrument SPOT_USD_CHF = new AtInstrument("#USD/CHF", Instruments.USD_CHF);
 	/** USD/JPY. */
-	public static final AtInstrument USD_JPY = new AtInstrument("#USD/JPY", Instruments.USD_JPY);
+	public static final AtInstrument SPOT_USD_JPY = new AtInstrument("#USD/JPY", Instruments.USD_JPY);
 
 	public static final Set<AtInstrument> values() {
 		Set<AtInstrument> set = new LinkedHashSet<>();
-		set.add(AUD_USD);
-		set.add(EUR_JPY);
-		set.add(EUR_USD);
-		set.add(GBP_USD);
-		set.add(NZD_USD);
-		set.add(USD_CAD);
-		set.add(USD_CHF);
-		set.add(USD_JPY);
+		set.add(SPOT_AUD_USD);
+		set.add(SPOT_EUR_JPY);
+		set.add(SPOT_EUR_USD);
+		set.add(SPOT_GBP_USD);
+		set.add(SPOT_NZD_USD);
+		set.add(SPOT_USD_CAD);
+		set.add(SPOT_USD_CHF);
+		set.add(SPOT_USD_JPY);
 		return set;
 	}
 
